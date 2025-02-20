5f11dc94a32eaa60e7748f7e1a02e10d62ff1056
Rutva L <rutva943@gmail.com>
2025-02-20 16:43:04 +0530

    CRUD

--- src/controllers/gadgets.ts
+++ src/controllers/gadgets.ts
@@ -1,36 +1,64 @@
 import { Request, Response } from "express";
+import { uniqueUsernameGenerator, Config, adjectives, nouns } from 'unique-username-generator'
+import db from "../config/database";
 import Gadgets from "../models/gadgets.models";
 
+const config: Config = {
+    dictionaries: [adjectives, nouns],
+    separator: ' ',
+    style: 'capital',
+  }
+
 export const getGadgets = async (req: Request, res: Response) => {
-    const gadgets = await Gadgets.findAll();
-    res.send(gadgets);
+    try {
+        const gadgets = await db.query(`SELECT *, CONCAT("name", ' - ', FLOOR(RANDOM()*100), '% success probability') AS missionSuccess FROM "Gadgets"`, {
+            model: Gadgets,
+            mapToModel: true,
+          });
+        res.send(gadgets);
+    } catch (error) {
+        res.send(error);
+    }
 };
 
 export const createGadget = async (req: Request, res: Response) => {
-    const { name } = req.body;
-    const newGadget = await Gadgets.create({ name });
-    res.send(newGadget);
+    try {
+        // const { name } = req.body;
+        const name = uniqueUsernameGenerator(config);
+        const newGadget = await Gadgets.create({ name: `The ${name}` });
+        res.send(newGadget);
+    } catch (error) {
+        res.send(error);
+    }
 };
 
 export const updateGadget = async (req: Request, res: Response) => {
-    const newGadget = await Gadgets.update(req.body, {
-        where: {
-            id: req.params.id
-        }
-    });
-    res.send(newGadget);
+    try {
+        const newGadget = await Gadgets.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.send(`gadget ${req.params.id} has been updated`);
+    } catch (error) {
+        res.send(error);
+    }
 };
 
 export const deleteGadget = async (req: Request, res: Response) => {
-    const newGadget = await Gadgets.update({
-        status: "Decommissioned",
-        decomissionedAt: new Date()
-    }, {
-        where: {
-            id: req.params.id
-        }
-    });
-    res.send(`gadget ${req.params.id} has been decommissioned`);
+    try {
+        const newGadget = await Gadgets.update({
+            status: "Decommissioned",
+            decomissionedAt: new Date()
+        }, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.send(`gadget ${req.params.id} has been decommissioned`);
+    } catch (error) {
+        res.send(error);
+    }
 };
 
 export const selfDestructGadget = async (req: Request, res: Response) => {
